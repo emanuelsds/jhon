e1c6ade474d01c07d7a246eb3c19a84db8fe6893
Carlos Emanuel <carlos@legaut.com>
2022-05-22 17:22:59 -0300

    jhon2

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
 a = [0]
-while True:
-    try:
-        numero = int(input("Numero: "))
-        numero2 = int(input("Numero 2: "))
-        print('\n')
-        if numero > a[-1] and numero2 == 2:
-            a.append(numero)
-        else:
-            print("BBB")
-        print(a)
-    except ValueError:
-         print("Algo deu errado")
+while
